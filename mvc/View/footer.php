e64484c72a5c999a40055fa25db352354366feb4
Yaamin0724 <yaminfaruque@gmail.com>
2020-09-09 17:24:09 +0600

    Project mvc

---
+++ mvc/View/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Online Library Management System, CSE 470 Assignment
+                </div>
+
+            </div>
+        </div>
+    </section>
